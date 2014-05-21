7eff014e84ddac07be2a0457b8e25efb436d33bb
omegasrevenge <kirill.salzmann@web.de>
2014-05-21 23:27:28 +0200

    Revert "Trying to Fix Unity Crash Bug"
    This reverts commit 464a6c3a645c857cc7181cee9111bf89dcaf7c66.

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -4,13 +4,10 @@ using System.Collections;
 public class GameManager : MonoBehaviour 
 {
 	public static GameManager Singleton;
-	[HideInInspector]
+
 	public GameObject RightSpawnPoint;
-	[HideInInspector]
 	public GameObject LeftSpawnPoint;
-	[HideInInspector]
 	public Transform MovementBeacon;
-	[HideInInspector]
 	public GameObject CharacterPrefab;
 
 	[HideInInspector]
@@ -21,10 +18,6 @@ public class GameManager : MonoBehaviour
 		Singleton = this;
 		Application.runInBackground = true;
 		Application.targetFrameRate = 60;
-		RightSpawnPoint = GameObject.Find("RightSpawnPoint");
-		LeftSpawnPoint = GameObject.Find("LeftSpawnPoint");
-		MovementBeacon = GameObject.Find("MovementBeacon").transform;
-		CharacterPrefab = Resources.Load<GameObject>("Character");
 	}
 
 	void Start()
